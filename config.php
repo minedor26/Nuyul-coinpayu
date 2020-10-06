023f9f7a581a0a0f20474ceee555d2f958590879
minedor26 <42230986+minedor26@users.noreply.github.com>
2020-10-06 16:58:07 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?
 
-$useragent = "xxxxxxx";
-$token = "xxxxxxx";
-$PHPSESSID = "PHPSESSID=xxxxxxx";
-$cfduid = "__cfduid=xxxxxxxxx";
+$useragent = "Mozilla/5.0 (Linux; Android 10; SM-A205W) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36";
+$token = "0a422537efa8ecdbae9c7a2ecc9499df38df484a";
+$PHPSESSID = "PHPSESSID=PHPSESSID=rpq9a3lpn1emk9jp2pq7j82fer";
+$cfduid = "__cfduid=defe5b37a2576fca4e2714b0dc7acbfa21602016619";
